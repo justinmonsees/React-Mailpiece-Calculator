ab726ce0465065ed80751fa5da33f7d67f5e615b
Justin <justin@Emilys-MacBook-Pro.local>
2023-01-05 11:35:44 -0500

    chore:Added connectivity to remote MongoDB database

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,3 +1,16 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/paper-db");
+mongoose.set("strictQuery", false);
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const MONGODB_USER = process.env.MONGODB_USER;
+const MONGODB_PASS = process.env.MONGODB_PASS;
+
+const DB_NAME = "PaperDB";
+
+const LOCAL_DB_CONNECTION = "mongodb://127.0.0.1:27017/paper-db";
+const REMOTE_DB_CONNECTION = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@cluster0.72t4jb2.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(REMOTE_DB_CONNECTION);
